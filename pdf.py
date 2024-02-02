bedad9be6666646b5083c58e27a65110fe7d1c90
ahmedelshaikh3 <158591897+ahmedelshaikh3@users.noreply.github.com>
2024-02-02 20:38:14 +0200

    Add files via upload

---
+++ pdf.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# Welcome to this interactive notebook,This notebook about Data Science Tools and Ecosystem are summarized.
+
+# [Python,R,Julia,C/C++]
+
+# [NumPy,Pandas,Matplotlib,TensorFlow,PyTorch]
+
+# |         Data Science tools      |
+# |---------------------------------|
+# |      Programming Languages      |
+# |           (IDEs)                |
+# |  Data Collection and Cleaning   |
+# |     Data Visualization          |
+# |       Machine Learning          |
+
+# Arithmetic operations in Python are Addition (+),Subtraction (-),Multiplication (*) and Division (/)
+
+# In[2]:
+
+
+(5*6)+8
+
+
+# In[6]:
+
+
+minutes = 200
+hours = minutes / 60
+
+print(hours)
+
+
+# ## Objectives in this course
+# 1-Master the most up-to-date practical skills and knowledge that data scientists use in their daily roles
+# 2-Learn the tools, languages, and libraries used by professional data scientists, including Python and SQL
+# 3-Import and clean data sets, analyze and visualize data, and build machine learning models and pipelines
+# 4-pply your new skills to real-world projects and build a portfolio of data projects that showcase your proficiency to employers
+
+#  
+
+#  
+
+# Author is Ahmed Elshiakh
+
+# In[ ]:
+
+
+
+
